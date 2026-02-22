d65f1f796f60d79785d116b817f01fe8f659beb0
cuong171207-lab <cuong171207@gmail.com>
2026-02-22 15:30:07 +0700

    new update 5

--- main.py
+++ main.py
@@ -34,32 +34,11 @@ def main():
         
         choice = input("Nhập lựa chọn của bạn: ").strip()
 
-        if choice == '1':
-            pm.display_all_products(products)
-        
-        elif choice == '2':
-            # Hàm add_product trả về list đã cập nhật (theo yêu cầu)
-            products = pm.add_product(products)
-        
-        elif choice == '3':
-            products = pm.update_product(products)
-            
-        elif choice == '4':
-            products = pm.delete_product(products)
-            
-        elif choice == '5':
-            pm.search_product_by_name(products)
-            
-        elif choice == '6':
-            pm.save_data(products)
-            
-        elif choice == '0':
         if choice == '0':
             print("Đang lưu dữ liệu trước khi thoát...")
             pm.save_data(products)
             print("Cảm ơn bạn đã sử dụng phần mềm POLYLAP. Tạm biệt!")
             break
-            
         elif choice in menu_actions:
             # Các chức năng 2, 3, 4 có thay đổi dữ liệu nên cần cập nhật lại biến products
             if choice in ['2', '3', '4']:
